b85f569b0fe7934a05fffb4301164c446dbfee1d
丁玲 <809183954@qq.com>
2020-09-03 11:03:07 +0800

    update React-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from './common/header';
 import {GlobalIcon} from './statics/iconfont/iconfont';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 
 class App extends Component {
@@ -9,8 +10,17 @@ class App extends Component {
     return (
       <>
       <Provider store={store}>
-        <Header/>
-        <GlobalIcon/>
+        <div>
+          <Header/>
+          <GlobalIcon/>
+          <BrowserRouter>
+            <div>
+            {/* exact路径完全相等才显示 */}
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
       </>
     )
